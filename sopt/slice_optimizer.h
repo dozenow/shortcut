75385b37b05ba7f9f65a269f826dd68d92cb70b3
Wesley Coomber <wcoomber@umich.edu>
2018-05-10 16:24:37 -0400

    some data struct improvements3

--- sopt/slice_optimizer.h
+++ sopt/slice_optimizer.h
@@ -2,24 +2,27 @@
 #include <stdlib.h>
 #include <vector>
 
-//add comments explain each struct, class
-//add flag that marks whether should be cleaned or not. flag and sweep garbage collection
-// two sets of edges (input edges and output edges)
+//A Node represents an instruction, such as "mov EAX, 20" by its line number in the assembly exslice1.c file
+//A Node has a vector of input Edges and a vector of output Edges
+//The bool extra flag is for the 'mark and sweep' tracing garbage collection to remove unnecessary slice instructions. If the node is marked as extra, then it is extraneous and unnecessary, so we can remove the instruction from the slice.
 struct Node {
 	int lineNum;
 	std::vector<Edge*> inEdges;
 	std::vector<Edge*> outEdges;
-	bool dirty = false;
+	bool extra = false;
 }
 
+//An Edge represents the relationship between two Nodes in our graph. Each edge has a pointer to its origin Node and a pointer to its destination Node.
+//We have a boolean taint flag to keep track of the tainted inputs into our instructions.
 struct Edge {
 	Node* start;
 	Node* finish;
+	bool taint = true;
 }
 
+//The directed acyclic Graph of instructions is represented by a vector of Nodes.
 struct instrGraph {
 	std::vector<Node*> nodes;
-	std::vector<Edge*> edges;
 }
 
 //change to use same taint format as pin
@@ -40,11 +43,13 @@ struct registers {
 	std::vector<tregister*>
 }
 
-struct memLoc {
-	ulong address;
-	std::vector<Node*>;
+//The memory state of our slice is represented by a map of 32-bit addresses and the Nodes that most recently affected the memory location at that address.
+struct memLocations {
+	std::map<ulong, Node*> mapMem;
 }
 
+//The 32-bit EFLAGS register is represented as a vector of nodes that most recently affected each byte of the EFLAGS register.
+//We have an explicit way to modify certain flag bits such as the "CF, Carry Flag" that is the first 0 bit of the EFLAGS register.  
 struct eflags {
 	std::vector<Node*>;
 
