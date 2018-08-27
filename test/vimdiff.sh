e89273084bdd45c5a5ad90e249992841d1a7e672
Xianzheng <xdou@umich.edu>
2018-08-27 12:44:13 -0400

    modify some scripts

--- test/vimdiff.sh
+++ test/vimdiff.sh
@@ -1,2 +1,2 @@
-vimdiff <(xxd /replay_logdb/rec_49218/ckpt.940.ckpt_mmap.$1) <(xxd /replay_logdb/rec_49224/ckpt.940.ckpt_mmap.$1)
+vimdiff <(xxd $1) <(xxd $2)
 #vimdiff <(xxd /replay_logdb/rec_45061/ckpt.1360.ckpt_mmap.$1) <(xxd /replay_logdb/rec_45065/ckpt.1360.ckpt_mmap.$1)
